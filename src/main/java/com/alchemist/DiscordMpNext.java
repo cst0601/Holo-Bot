625c49b2db5ac9ac20d98215dca983511c14be2e
Neuenmuller <greg870601@gmail.com>
2020-09-29 00:01:36 +0800

    removed test token (and renewed)

--- src/main/java/com/alchemist/DiscordMpNext.java
+++ src/main/java/com/alchemist/DiscordMpNext.java
@@ -14,13 +14,7 @@ import com.alchemist.service.VtubeListener;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
-import twitter4j.Query;
-import twitter4j.QueryResult;
-import twitter4j.Status;
-import twitter4j.Twitter;
-import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
-import twitter4j.conf.ConfigurationBuilder;
+
 
 public class DiscordMpNext {
 	private JDA jda;
