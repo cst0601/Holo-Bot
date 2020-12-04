b20ba5491a052c36a4ef775e4fc0048e3be662df
Neuenmuller <greg870601@gmail.com>
2020-12-05 02:09:52 +0800

    update bonk method

--- src/main/java/com/alchemist/service/BonkListener.java
+++ src/main/java/com/alchemist/service/BonkListener.java
@@ -1,19 +1,31 @@
 package com.alchemist.service;
 
-import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.MessageBuilder;
 import net.dv8tion.jda.api.entities.ChannelType;
+import net.dv8tion.jda.api.entities.Emote;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.MessageHistory;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.exceptions.ErrorHandler;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.ErrorResponse;
 
+import java.util.EnumSet;
+import java.util.LinkedList;
 import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.alchemist.ArgParser;
 
+
 public class BonkListener extends ListenerAdapter implements Service {
+	
+	private Logger logger = LoggerFactory.getLogger(BonkListener.class);
+	private final String BONK_EMOTE_ID = "756142081864630343";	// TODO: remove this literal constant
+	private Emote emote = null;
 	
 	@Override
 	public void onMessageReceived(MessageReceivedEvent event) {
@@ -26,21 +38,43 @@ public class BonkListener extends ListenerAdapter implements Service {
 			ArgParser parser = new ArgParser(msg);
 			
 			if (parser.getCommand().equals(">bonk")) {
-				List<Member> mentionedMember = message.getMentionedMembers();
-				if (mentionedMember.size() > 0) {
-					
-					EmbedBuilder embedBuilder = new EmbedBuilder()
-							.setImage("https://i.imgur.com/lmrPJjL.png");
-					
-					MessageBuilder messageBuilder = new MessageBuilder(embedBuilder);
-					
-					messageBuilder.append(":white_check_mark: ");
-					for (Member member: mentionedMember)
-						messageBuilder.append(member.getAsMention() + " ");
-					messageBuilder.append("is bonked");
-					
-					channel.sendMessage(messageBuilder.build()).queue();
+				
+				emote = message.getGuild().getEmoteById(BONK_EMOTE_ID);
+				
+				if (emote != null) {
+					List<Member> mentionedMember = new LinkedList<>(message.getMentionedMembers());
+					getTargetMessage(channel, message.getId(), mentionedMember);
 				}
+				else {
+					logger.warn(String.format(
+							"Requested from guild: %s, does not contain designated emote.",
+							message.getGuild().getId()));
+					channel.sendMessage("Error: This sever does not contain the designated bonk emote.").queue();
+				}
+				
+			}
+		}
+	}
+	
+	private void getTargetMessage(
+			MessageChannel channel,
+			String messageId,
+			List<Member> targetMember)
+	{	
+		// this is a rest action that waits for completion, might make this method to thread
+		MessageHistory history = MessageHistory.getHistoryBefore(channel, messageId)
+				.limit(10).complete();
+		for(Message message: history.getRetrievedHistory()) {
+			if (targetMember.contains(message.getMember())) {
+				logger.info("target aquired");
+				targetMember.remove(message.getMember());
+				message.addReaction(emote).queue(
+						null,
+						new ErrorHandler().handle(
+								EnumSet.of(ErrorResponse.MISSING_PERMISSIONS),
+								(ex) -> channel.sendMessage("Action failed due to missing permission.")
+							)
+						);
 			}
 		}
 	}
